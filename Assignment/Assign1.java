9569884c95de10d00d49e1095d672c9a00277365
Vedant Sharma <49811129+VedantSharma15@users.noreply.github.com>
2022-05-04 19:25:56 +0530

    Add files via upload

---
+++ Assignment/Assign1.java
@@ -0,0 +1,21 @@
+
+public class Assign1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1,num2;
+		double sum,sub,mul,div,rem;
+		num1=3000;
+		num2=2000;
+		sum=num1+num2;
+		sub=num1-num2;
+		mul=num1*num2;
+		div=num1/num2;
+		rem=num1%num2;
+		System.out.println("Sumation= "+sum);
+		System.out.println("Subtraction= "+sub);
+		System.out.println("Multiplication= "+mul);
+		System.out.println("Division= "+div);
+		System.out.println("Remainder= "+rem);
+	}
+}
