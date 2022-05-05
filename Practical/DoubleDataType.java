323444951d82d51ad354fea33b93ebfd9b765d71
Vedant Sharma <49811129+VedantSharma15@users.noreply.github.com>
2022-05-05 17:53:47 +0530

    Add files via upload

---
+++ Practical/DoubleDataType.java
@@ -0,0 +1,13 @@
+public class DoubleDataType
+{
+	public static void main(String args[])
+	{
+		double myDouble1, myDouble2, result;
+		myDouble1 = 12345.6789d;
+		myDouble2 = 98765.4321d;
+		result = myDouble1 + myDouble2;
+		System.out.println("Number 1: " +myDouble1);
+		System.out.println("Number 2: " +myDouble2);
+		System.out.println("Number 1 + Number 2: " +result);
+	}
+}
