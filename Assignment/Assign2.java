70015e6d620ffcbaa3a196914e0715bf9ac5b1b9
Vedant Sharma <49811129+VedantSharma15@users.noreply.github.com>
2022-05-05 18:07:17 +0530

    Add files via upload

---
+++ Assignment/Assign2.java
@@ -0,0 +1,22 @@
+public class Assign2
+{
+	public static void main(String[] args)
+	{
+		byte a=25;
+		char b='x';
+		int c=125;
+		double d=123456D;
+		long e=11111L;
+		float f=123.456F;
+		short g=8;
+		boolean h=false;
+		System.out.println("Byte data= "+a);
+		System.out.println("Char data= "+b);
+		System.out.println("Int data= "+c);
+		System.out.println("Double data= "+d);
+		System.out.println("Long data= "+e);
+		System.out.println("Float data= "+f);
+		System.out.println("Short data= "+g);
+		System.out.println("Boolean data= "+h);
+	}
+}
