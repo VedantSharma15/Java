20263b70b4e490c0f88a4145c515bdf12224c51f
Vedant Sharma <49811129+VedantSharma15@users.noreply.github.com>
2022-05-05 17:47:46 +0530

    Add files via upload

---
+++ Practical/BooleanDataType.java
@@ -0,0 +1,9 @@
+public class BooleanDataType
+{
+	public static void main(String args[])
+	{
+		boolean myBool = true;
+		if(myBool == true)
+		System.out.println("I am using a Boolean data type:"+myBool);
+	}
+}
