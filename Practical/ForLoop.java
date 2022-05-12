d139913386bdd71c426ea446a2c0b20c9ed52d7b
Vedant Sharma <49811129+VedantSharma15@users.noreply.github.com>
2022-05-12 18:35:14 +0530

    Add files via upload

---
+++ Practical/ForLoop.java
@@ -0,0 +1,13 @@
+public class ForLoop
+{
+	public static void main(String[] args)
+	{
+		int a[]={1,2,3,4,5};
+		int sum=0;
+		for(int b:a)
+		{
+			sum+=b;
+		}
+		System.out.println("Sum of array elements="+sum);
+		}
+}
---
+++ Practical/WhileLoop.java
@@ -0,0 +1,14 @@
+public class WhileLoop
+{
+	public static void main(String[] args)
+	{
+		int sum=0;
+		int i=0;
+		while(i<=10)
+		{
+			sum+=i;
+			i++;
+		}
+		System.out.println("Sum of array elements="+sum);
+	}
+}
